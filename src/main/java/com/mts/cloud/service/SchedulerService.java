61e379173ad6af5b6873a22c7d36867f5816fc01
vnaumkin <naumkin.vlad2012@yandex.ru>
2023-10-13 11:29:23 +0300

    refactor: изменил optimal_load

--- src/main/java/com/mts/cloud/service/SchedulerService.java
+++ src/main/java/com/mts/cloud/service/SchedulerService.java
@@ -21,11 +21,7 @@ import java.util.concurrent.TimeUnit;
 public class SchedulerService {
 
     private static final Logger log = LoggerFactory.getLogger(SchedulerService.class);
-
-    public static final int MAX_LOAD = 85;
-    public static final int MIN_VM_LOAD = 50;
-    public static final int MIN_DB_LOAD = 30;
-    public static final int OPTIMAL_LOAD = 60;
+    public static final int OPTIMAL_LOAD = 75;
     public static final int MIN_OPTIMAL_LOAD = 70;
     public static final int MAX_OPTIMAL_LOAD = 80;
 
@@ -42,8 +38,6 @@ public class SchedulerService {
         List<GetResource> resources = resourceService.get();
         calculateDb2(getOptimalDb(statistic), statistic, resources);
         calculateVm2(getOptimalVm(statistic), statistic, resources);
-//        calculateDb(statistic, resources);
-//        calculateVm(statistic, resources);
     }
 
     public void calculateDb2(int optimalDb, Statistic statistic, List<GetResource> resources) {
@@ -150,99 +144,6 @@ public class SchedulerService {
                 }
             }
         }
-    }
-
-    private void calculateDb(Statistic statistic, List<GetResource> resources) {
-        double dbCpuLoad = statistic.dbCpuLoad();
-        double dbRamLoad = statistic.dbRamLoad();
-        log.debug("db_cpu_load = {}  db_ram_load = {}", dbCpuLoad, dbRamLoad);
-        List<GetResource> listDb = resources.stream()
-                .filter(r -> r.type().equals(ResourceType.DB))
-                .toList();
-        boolean isFailed = listDb.stream()
-                .anyMatch(GetResource::failed);
-        if (isFailed) {
-            return;
-        }
-        if (dbRamLoad == 0) {
-            Price price = priceClient.getMax(ResourceType.DB);
-            resourceService.add(price);
-        } else if (dbCpuLoad >= MAX_LOAD || dbRamLoad >= MAX_LOAD) {
-            Price price = priceClient.getMax(ResourceType.DB);
-            GetResource getResource = getMin(listDb);
-            if (getResource == null) {
-                resourceService.add(price);
-            } else {
-                resourceService.update(getResource.id(), price);
-            }
-        } else if (dbRamLoad <= MIN_DB_LOAD && !listDb.isEmpty()) {
-            if (listDb.size() == 1) {
-                Price price = priceClient.getMax(ResourceType.DB);
-                resourceService.update(listDb.get(0).id(), price);
-            } else {
-                resourceService.delete(ResourceType.DB);
-            }
-        } else if (dbRamLoad < OPTIMAL_LOAD) {
-            GetResource getResource = getMax(listDb);
-            if (getResource == null) {
-                resourceService.delete(ResourceType.DB);
-            } else {
-                Price price = priceClient.getMin(ResourceType.DB);
-                resourceService.update(getResource.id(), price);
-            }
-        }
-    }
-
-    private void calculateVm(Statistic statistic, List<GetResource> resources) {
-        double vmCpuLoad = statistic.vmCpuLoad();
-        double vmRamLoad = statistic.vmRamLoad();
-        log.debug("vm_cpu_load = {}  vm_ram_load = {}", vmCpuLoad, vmRamLoad);
-        List<GetResource> listVm = resources.stream()
-                .filter(r -> r.type().equals(ResourceType.VM))
-                .toList();
-        boolean isFailed = listVm.stream()
-                .anyMatch(GetResource::failed);
-        if (isFailed) {
-            return;
-        }
-        if (vmRamLoad == 0) {
-            Price price = priceClient.getMax(ResourceType.VM);
-            resourceService.add(price);
-        } else if (vmCpuLoad >= MAX_LOAD || vmRamLoad >= MAX_LOAD) {
-            Price price = priceClient.getMax(ResourceType.VM);
-            GetResource getResource = getMin(listVm);
-            if (getResource == null) {
-                resourceService.add(price);
-            } else {
-                resourceService.update(getResource.id(), price);
-            }
-        } else if (vmRamLoad <= MIN_VM_LOAD && !listVm.isEmpty()) {
-            if (listVm.size() == 1) {
-                Price price = priceClient.getMax(ResourceType.VM);
-                resourceService.update(listVm.get(0).id(), price);
-            }
-            resourceService.delete(ResourceType.VM);
-        } else if (vmRamLoad < OPTIMAL_LOAD) {
-            GetResource getResource = getMax(listVm);
-            if (getResource == null) {
-                resourceService.delete(ResourceType.VM);
-            } else {
-                Price price = priceClient.getMin(ResourceType.VM);
-                resourceService.update(getResource.id(), price);
-            }
-        }
-    }
-
-    public GetResource getMin(List<GetResource> list) {
-        return list.stream()
-                .min(Comparator.comparingInt(GetResource::ram))
-                .orElse(null);
-    }
-
-    public GetResource getMax(List<GetResource> list) {
-        return list.stream()
-                .max(Comparator.comparingInt(GetResource::ram))
-                .orElse(null);
     }
 
     public int getOptimalDb(Statistic statistic) {
